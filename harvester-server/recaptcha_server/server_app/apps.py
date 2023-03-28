d730ccb7b16cfd749b13f7907ca5f8a88a913997
Ben Stahl <48197017+benastahl@users.noreply.github.com>
2023-03-28 13:17:01 -0400

    Delete harvester-server/recaptcha_server directory

--- harvester-server/recaptcha_server/server_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ServerAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'server_app'
--- harvester-server/recaptcha_server/server_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from django.contrib import admin
-
-from . import views
-
-urlpatterns = [
-    path('', views.waiting_for_captcha),
-    path('loading', views.load)
-]
